4b5b578c3c5b93b3b9023a0055481efd13575dfd
84054 <840542740@qq.com>
2022-06-28 19:10:26 +0800

    commit

---
+++ src/GitDemo.java
@@ -0,0 +1,11 @@
+/**
+ * @author 84054
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 2022/6/28 19:04
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("gitDemo test...");
+    }
+}
